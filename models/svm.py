3831e8edda549fd44b1bf3e74ebb0db2082d241a
GregShiner <greg@justifinetwork.com>
2024-04-23 20:12:28 -0400

    Cleanup code

--- models/svm.py
+++ models/svm.py
@@ -1,24 +1,26 @@
+import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 import pandas as pd
 # If the file is not already un-gzipped, unzip it
-# import gzip
-# import os
-# if not os.path.exists('recipes-vectorized.parquet'):
-#     with open('recipes-vectorized.parquet', 'wb') as f:
-#         with gzip.open('recipes-vectorized.parquet.gz', 'rb') as f_gz:
-#             f.write(f_gz.read())
-df_vectorized = pd.read_parquet('recipes-vectorized.parquet') # Read the vectorized DataFrame from the parquet file
+import gzip
+import os
+if not os.path.exists('recipes-vectorized.parquet'):
+    with open('recipes-vectorized.parquet', 'wb') as f:
+        with gzip.open('recipes-vectorized.parquet.gz', 'rb') as f_gz:
+            f.write(f_gz.read())
+# Read the vectorized DataFrame from the parquet file
+df_vectorized = pd.read_parquet('recipes-vectorized.parquet')
 
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
-# grid_search_svm = GridSearchCV(SVC(), {'C': [0.1, 1, 10, 100], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid']}, verbose=3, n_jobs=-1, cv=3)
+grid_search_svm = GridSearchCV(SVC(), {'C': [0.1, 1, 10, 100], 'kernel': [
+                               'linear', 'poly', 'rbf', 'sigmoid']}, verbose=3, n_jobs=-1, cv=3)
 # Sample 10%
-# df_sample = df_vectorized.sample(frac=0.1)
-# X = df_sample['KeywordsVector'].to_list()
-# y = df_sample['Healthy']
-# grid_search_svm.fit(X, y)
-import pickle
-# with open('classifier_svm.pkl', 'wb') as f:
-#     pickle.dump(grid_search_svm, f)
+df_sample = df_vectorized.sample(frac=0.1)
+X = df_sample['KeywordsVector'].to_list()
+y = df_sample['Healthy']
+grid_search_svm.fit(X, y)
+with open('classifier_svm.pkl', 'wb') as f:
+    pickle.dump(grid_search_svm, f)
 
 X_pca_90 = pd.read_parquet('parquets/recipes-pca-90.parquet')
 X_pca_95 = pd.read_parquet('parquets/recipes-pca-95.parquet')
@@ -40,7 +42,8 @@ X_pca_90_sample = X_pca_90_sample.drop(columns='Healthy')
 X_pca_95_sample = X_pca_95_sample.drop(columns='Healthy')
 X_pca_99_sample = X_pca_99_sample.drop(columns='Healthy')
 
-grid_search_svm = GridSearchCV(SVC(), {'C': [0.1, 1, 10, 100], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid']}, verbose=3, n_jobs=-1, cv=3)
+grid_search_svm = GridSearchCV(SVC(), {'C': [0.1, 1, 10, 100], 'kernel': [
+                               'linear', 'poly', 'rbf', 'sigmoid']}, verbose=3, n_jobs=-1, cv=3)
 
 print('Fitting svm with pca 90')
 grid_search_svm.fit(X_pca_90_sample, y)
--- models/xgb.py
+++ models/xgb.py
@@ -1,3 +1,6 @@
+import pickle
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBClassifier
 import pandas as pd
 # If the file is not already un-gzipped, unzip it
 import gzip
@@ -6,19 +9,18 @@ if not os.path.exists('recipes-vectorized.parquet'):
     with open('recipes-vectorized.parquet', 'wb') as f:
         with gzip.open('recipes-vectorized.parquet.gz', 'rb') as f_gz:
             f.write(f_gz.read())
-df_vectorized = pd.read_parquet('recipes-vectorized.parquet') # Read the vectorized DataFrame from the parquet file
+# Read the vectorized DataFrame from the parquet file
+df_vectorized = pd.read_parquet('recipes-vectorized.parquet')
 
-from xgboost import XGBClassifier
-from sklearn.model_selection import GridSearchCV
-# grid_search_xgb = GridSearchCV(XGBClassifier(), {'n_estimators': [100, 200, 300], 'max_depth': [3, 5, 7]}, verbose=3, n_jobs=-1, cv=3)
+grid_search_xgb = GridSearchCV(XGBClassifier(), {'n_estimators': [
+                               100, 200, 300], 'max_depth': [3, 5, 7]}, verbose=3, n_jobs=-1, cv=3)
 # Sample 10%
-# df_sample = df_vectorized.sample(frac=0.1)
-# X = df_sample['KeywordsVector'].to_list()
-# y = df_sample['Healthy']
-# grid_search_xgb.fit(X, y)
-import pickle
-# with open('classifier_xgb.pkl', 'wb') as f:
-#     pickle.dump(grid_search_xgb, f)
+df_sample = df_vectorized.sample(frac=0.1)
+X = df_sample['KeywordsVector'].to_list()
+y = df_sample['Healthy']
+grid_search_xgb.fit(X, y)
+with open('classifier_xgb.pkl', 'wb') as f:
+    pickle.dump(grid_search_xgb, f)
 
 X_pca_90 = pd.read_parquet('parquets/recipes-pca-90.parquet')
 X_pca_95 = pd.read_parquet('parquets/recipes-pca-95.parquet')
@@ -26,7 +28,8 @@ X_pca_99 = pd.read_parquet('parquets/recipes-pca-99.parquet')
 
 y = df_vectorized['Healthy']
 
-grid_search_xgb = GridSearchCV(XGBClassifier(), {'n_estimators': [100, 200, 300], 'max_depth': [3, 5, 7]}, verbose=3, n_jobs=-1, cv=3)
+grid_search_xgb = GridSearchCV(XGBClassifier(), {'n_estimators': [
+                               100, 200, 300], 'max_depth': [3, 5, 7]}, verbose=3, n_jobs=-1, cv=3)
 
 print('Fitting xgb with pca 90')
 grid_search_xgb.fit(X_pca_90, y)
@@ -37,7 +40,7 @@ print('Fitting xgb with pca 95')
 grid_search_xgb.fit(X_pca_95, y)
 with open('classifier_xgb_pca_95.pkl', 'wb') as f:
     pickle.dump(grid_search_xgb, f)
-    
+
 print('Fitting xgb with pca 99')
 grid_search_xgb.fit(X_pca_99, y)
 with open('classifier_xgb_pca_99.pkl', 'wb') as f:
